7452ee5a52dfbe08390e00b7f936e203126fffc5
Avorent <panlau98@gmail.com>
2018-01-26 01:43:23 -0500

    .

--- Queries/SQLQueries.sql
+++ Queries/SQLQueries.sql
@@ -1,11 +1,45 @@
--- Find the contacts of an instructor of a certain course (Course -> Course info -> Instructor -> Instructor info) 
+-- Find the contacts of an instructor of a certain course (Course -> Course info -> Instructor -> Instructor info)
 SELECT email, phone_num
 FROM user
 -- instructor only
 WHERE role_id = 2
   {
-  SELECT user 
+  SELECT user
   };
 
+  -- Selects all information about the registered courses for the instructor's homepage.
+SELECT *
+FROM course_sections se
+LEFT JOIN course_information ci
+ON se.course_num = ci.course_num
+WHERE se.id
+IN(
+  SELECT section_id
+  FROM instructor_course_link_registered
+  WHERE instructor_id
+  IN(
+    SELECT id
+    FROM instructors
+    WHERE user_id = ?
+  )
+)
+ORDER BY se.id ASC;
 
--- Check for schedule conflicts (Course -> Course Info -> Course Section/Days/Time (compare ‘brute force’-like))
+--SIMILAR
+-- Selects all information about the pending courses for the instructor's homepage.
+SELECT *
+FROM course_sections se
+LEFT JOIN course_information ci
+ON se.course_num = ci.course_num
+WHERE se.id
+IN(
+  SELECT section_id
+  FROM instructor_course_link_cart
+  WHERE instructor_id
+  IN(
+    SELECT id
+    FROM instructors
+    WHERE user_id = ?
+  )
+)
+ORDER BY se.id ASC;
