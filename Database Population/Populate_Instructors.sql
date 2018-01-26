197e14d166eeb1d5a07b3d9139e3693eaf0a2f89
Avorent <panlau98@gmail.com>
2018-01-26 11:03:25 -0500

    Moved population scripts to a folder

---
+++ Database Population/Populate_Instructors.sql
@@ -0,0 +1,94 @@
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(1,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(2,4);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(3,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(4,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(5,1);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(6,2);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(7,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(8,4);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(9,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(10,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(11,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(12,2);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(13,0);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(14,2);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(15,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(16,4);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(17,2);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(18,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(19,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(20,1);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(21,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(22,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(23,0);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(24,4);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(25,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(26,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(27,4);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(28,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(29,0);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(30,1);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(31,2);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(32,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(33,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(34,1);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(35,2);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(36,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(37,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(38,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(39,4);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(40,2);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(41,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(42,1);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(43,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(44,0);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(45,1);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(46,3);
+INSERT INTO instructors(instructor_id,req_courses)
+VALUES(47,3);
